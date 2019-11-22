cb9bd57ebb06e62c895165b21a0fe76fea418d22
Nikita S Modi <nikita.s.modi@outlook.com>
2019-11-22 19:30:59 +0530

    Updated Code with Post Method

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, jsonify
-import pandas as pd
-
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+import pandas as pd
+from pandas import DataFrame
+
 
 @app.route("/data")
 def movielens():
-    data = pd.read_csv("file:///C:/Users/user/Projects/MovieLens/ml-100k/u.item" , sep="|")
-    data = data.filter(["Movie_ID", "Movie_Title"])
-    return data.to_json(orient='values')
+    data = pd.read_csv("file:///C:/Users/user/Projects/MovieLens/ml-100k/u.item", sep="|")
+    data = DataFrame(data, columns=['Movie_ID', 'Movie_Title'])
+    return data.to_dict(orient='index')
 
+
+@app.route("/pushdata", methods=['POST'])
+def get_from_frontend():
+    data = request.get_json('username')
+    print(data)
+    return data
 
 
 if __name__ == "__main__":
